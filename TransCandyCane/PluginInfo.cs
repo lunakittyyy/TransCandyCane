961b51a2a982a34d71ae20a8745e14be6bb8acdf
Luna <lennon.m.craig@gmail.com>
2023-05-11 17:26:58 -0400

    unfuck the repo

--- TransCandyCane/PluginInfo.cs
+++ TransCandyCane/PluginInfo.cs
@@ -1,8 +1,8 @@
-namespace TransCandyCane
+﻿namespace TransCandyCane
 {
     internal class PluginInfo
     {
-        public const string GUID = "org.luna.gorillatag.transcandycane";
+        public const string GUID = "com.luna.gorillatag.transcandycane";
         public const string Name = "TransCandyCane";
         public const string Version = "1.0.0";
     }
