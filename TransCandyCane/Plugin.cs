db768a9bf8fff444ff479c5f8cd0e5fd1127a470
Luna <lennon.m.craig@gmail.com>
2023-05-31 11:45:11 -0400

    get real

--- TransCandyCane/Plugin.cs
+++ TransCandyCane/Plugin.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 using HarmonyLib;
 using System.Threading.Tasks;
 using System.Linq;
+using Utilla;
+using System;
 
 namespace TransCandyCane
 {
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     public class Plugin : BaseUnityPlugin
     {
         private static Texture2D TransCandyCaneTex;
         private Harmony Harmony;
+        
+        // TODO: make this not use Utilla for the fix
+        // perhaps we need to make Awake run asyncronously for the game to not lock up?
+        void Awake()
+        {
+            Utilla.Events.GameInitialized += GameInitialized;
+        }
 
-        public void Awake()
+        public void GameInitialized(object sender, EventArgs e)
         {
             var str = Assembly.GetExecutingAssembly().GetManifestResourceStream("TransCandyCane.Resources.transcandycanetex");
             var bundle = AssetBundle.LoadFromStream(str);
--- TransCandyCane/PluginInfo.cs
+++ TransCandyCane/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.luna.gorillatag.transcandycane";
         public const string Name = "TransCandyCane";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
 }
